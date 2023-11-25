f87197379d2e00ab560e69513ba1e15bcf0ce47a
Antoine Putmans <palfw@Ok.local>
2023-11-25 01:29:36 +0100

    adding backend

---
+++ backend/eureka/src/main/java/be/vinci/ipl/amazing/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package be.vinci.ipl.amazing.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // Eureka Server
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
