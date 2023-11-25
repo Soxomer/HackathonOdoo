a5a4aa1dd938e4cfd543e6ec6c0f9cac57bf8d5d
guimochi <yamamoto.guilherme@outlook.com>
2023-11-25 13:43:37 +0100

    adding paths

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,15 @@ const app = express()
 const port = 3000
 const GitHubStrategy = require('passport-github').Strategy;
 const session = require('express-session');
+const {PrismaClient} = require("@prisma/client");
+
+const prisma = new PrismaClient()
 
 // Configure express-session
 app.use(session({
-    secret: 'Sup3rS3cur3!', // Use a strong and unique secret key
-    resave: true,
-    saveUninitialized: true,
+  secret: 'Sup3rS3cur3!', // Use a strong and unique secret key
+  resave: true,
+  saveUninitialized: true,
 }));
 
 app.use(passport.initialize());
@@ -17,58 +20,120 @@ app.use(passport.session());
 
 // Serialize and deserialize user
 passport.serializeUser((user, done) => {
-    done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser((obj, done) => {
-    done(null, obj);
+  done(null, obj);
 });
 
-passport.use(new GitHubStrategy({
-    clientID: "Iv1.bffad14f85e00390",
-    clientSecret: "14f9ed89365cdd5330d5023f117ddbad643f1a87",
-    callbackURL: "http://localhost:3000/auth/github/callback"
-  }, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
-    return done(null, profile);
-  }
+passport.use(new GitHubStrategy( {
+      clientID: "Iv1.bffad14f85e00390",
+      clientSecret: "14f9ed89365cdd5330d5023f117ddbad643f1a87",
+      callbackURL: "http://localhost:3000/auth/github/callback"
+    }, async (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+      let user = await prisma.user.findUnique({
+        where: {
+          id: profile.id,
+        },
+      });
+      if (await user) return done(null, profile);
+
+      user = prisma.user.create({
+        data: {
+          id: profile.id,
+          pseudo: profile.username,
+          token: accessToken,
+        },
+      });
+  console.log( await user);
+      return done(null, profile);
+    }
 ));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!')
+})
 
 app.get('/auth/github',
-  passport.authenticate('github'));
+    passport.authenticate('github'));
 
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-});
-
+app.get('/auth/github/callback',
+    passport.authenticate('github', {failureRedirect: '/login'}),
+    function (req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    });
 
 // Logout route
 app.get('/logout', (req, res) => {
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        res.redirect('/');
-    });
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 });
 
 // Profile route
 app.get('/profile', (req, res) => {
-    // Check if the user is authenticated
-    if (req.isAuthenticated()) {
-        res.send(`<h1>Hello ${req.user.username}</h1><a href="/logout">Logout</a>`);
-    } else {
-        res.redirect('/');
-    }
+  // Check if the user is authenticated
+  if (req.isAuthenticated()) {
+    console.log("is authenticated");
+    res.send(`<h1>Hello ${req.user.username}</h1><a href="/logout">Logout</a>`);
+  } else {
+    res.redirect('/');
+  }
 });
 
+
+// Create a new event
+app.post('/event/', (req, res) => {
+  const {name, date, description, location} = req.body;
+  const event = prisma.event.create({
+    data: {
+      name: name,
+      date: date,
+      description: description,
+      location: location,
+    },
+  });
+  res.json(event);
+});
+
+// Count the number of events from all users and them by the number of events
+app.get('/ranking/users', (req, res) => {
+  prisma.user.findMany({
+    include: {
+      events: true,
+    },
+  }).then((users) => {
+    const usersWithEvents = users.map((user) => {
+      return {
+        ...user,
+        eventsCount: user.events.length,
+      };
+    });
+    const sortedUsers = usersWithEvents.sort((a, b) => {
+      return b.eventsCount - a.eventsCount;
+    });
+    let usr = sortedUsers.map((user) => {
+      return {
+        pseudo: user.pseudo,
+        eventsCount: user.eventsCount,
+      };
+    });
+    res.json(usr);
+  });
+});
+
+
+
+// Start server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
---
+++ backend/prisma/migrations/20231125113507_init/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Event" DROP CONSTRAINT "Event_creatorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Event" ALTER COLUMN "creatorId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
