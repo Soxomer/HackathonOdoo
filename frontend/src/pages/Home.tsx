142dbfa277111cf9a12a8f9a3293518de0cdc723
john doe <gabriel.torres.silva@hotmail.com>
2023-11-25 11:27:54 +0100

    axios

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,26 @@
 import { IonGrid, IonRow, IonCol, IonIcon, IonButton, IonContent, IonPage } from '@ionic/react';
 import { logoGithub } from 'ionicons/icons';
+import axios from 'axios';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const fetchDataFromApi = async () => {
+    const url = 'https://127.0.0.1:3000/';
+
+    try {
+      const response = await axios.get(url);
+      console.log('Fetched data:', response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
   return (
     <IonPage>
       <IonContent>
         <IonGrid fixed={true}>
           <IonRow class="ion-justify-content-center">
             <IonCol size="12" size-md="4">
-            <IonButton expand="full" shape="round" color="dark">
+            <IonButton onClick={fetchDataFromApi} expand="full" shape="round" color="dark">
               <IonIcon slot="start" icon={ logoGithub }></IonIcon>
               Sign in with Github
             </IonButton>
